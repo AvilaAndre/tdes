d8588903222668a82217d827af94607cf013efc2
AvilaAndre <avila_andre14@hotmail.com>
2025-05-30 11:51:52 +0100

    feat: implemented math helper functions

--- src/simulations/generalized_linear_model/utils.rs
+++ src/simulations/generalized_linear_model/utils.rs
@@ -11,6 +11,45 @@ pub fn sqrt_elemwise(a: &Mat<f64>) -> Mat<f64> {
     Mat::from_fn(a.nrows(), a.ncols(), |i, j| a.get(i, j).sqrt())
 }
 
+pub fn sqr_elemwise(a: &Mat<f64>) -> Mat<f64> {
+    Mat::from_fn(a.nrows(), a.ncols(), |i, j| a.get(i, j) * a.get(i, j))
+}
+
+/*
+ * This method also broadcasts if possible
+ */
+pub fn mul_elementwise(a: &Mat<f64>, b: &Mat<f64>) -> Mat<f64> {
+    let (nrows, ncols) = (a.nrows(), a.ncols());
+    let (brows, bcols) = (b.nrows(), b.ncols());
+
+    match (nrows, ncols, brows, bcols) {
+        // Direct element-wise multiplication
+        (ar, ac, br, bc) if ar == br && ac == bc => {
+            Mat::from_fn(ar, ac, |i, j| a.get(i, j) * b.get(i, j))
+        }
+        // Column broadcast on b
+        (ar, ac, br, 1) if ar == br => Mat::from_fn(ar, ac, |i, j| a.get(i, j) * b.get(i, 0)),
+        // Column broadcast on a
+        (ar, 1, br, bc) if ar == br => Mat::from_fn(br, bc, |i, j| a.get(i, 0) * b.get(i, j)),
+        // Row broadcast on b
+        (ar, ac, 1, bc) if ac == bc => Mat::from_fn(ar, ac, |i, j| a.get(i, j) * b.get(0, j)),
+        // Row broadcast on a
+        (1, ac, br, bc) if ac == bc => Mat::from_fn(br, bc, |i, j| a.get(0, j) * b.get(i, j)),
+        // Scalar broadcast on b
+        (ar, ac, 1, 1) => {
+            let val = b.get(0, 0);
+            Mat::from_fn(ar, ac, |i, j| a.get(i, j) * val)
+        }
+        // Scalar broadcast on a
+        (1, 1, br, bc) => {
+            let val = a.get(0, 0);
+            Mat::from_fn(br, bc, |i, j| a.get(i, j) * val)
+        }
+        // Incompatible shapes
+        _ => panic!("Cannot multiply matrices element-wise due to incompatible shapes"),
+    }
+}
+
 pub enum CatDim {
     VERTICAL = 0,
     HORIZONTAL = 1,
