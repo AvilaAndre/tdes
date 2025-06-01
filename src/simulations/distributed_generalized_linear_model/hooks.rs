110de74f0e0626cc2d9b59a4124842bbd04f6073
AvilaAndre <avila_andre14@hotmail.com>
2025-06-01 21:53:08 +0100

    feat: check if distributed generalized linear model simulation is correct

--- src/simulations/distributed_generalized_linear_model/hooks.rs
+++ src/simulations/distributed_generalized_linear_model/hooks.rs
@@ -1,39 +1,31 @@
 use faer::Mat;
 
 use crate::{
-    internal::context::{Context, CustomHook},
-    simulations::distributed_generalized_linear_model::peer::GlmPeer,
+    internal::context::CustomHook,
+    simulations::distributed_generalized_linear_model::{
+        peer::GlmPeer, utils::mat_allclose_default,
+    },
 };
 
 pub fn on_simulation_finish_hook(central: Mat<f64>) -> CustomHook {
-    Box::new(move |ctx| inner_func(ctx, &central))
-}
+    Box::new(move |ctx| {
+        let coefficients: Vec<Mat<f64>> = ctx
+            .peers
+            .iter()
+            .filter_map(|p| {
+                p.downcast_ref::<GlmPeer>()
+                    .map(|peer| peer.state.model.coefficients.clone())
+            })
+            .collect();
 
-fn inner_func(ctx: &Context, central: &Mat<f64>) {
-    let coefficients: Vec<Mat<f64>> = ctx
-        .peers
-        .iter()
-        .filter_map(|p| {
-            p.downcast_ref::<GlmPeer>()
-                .map(|peer| peer.state.model.coefficients.clone())
-        })
-        .collect();
-
-    check(central.clone(), coefficients);
-
-    //println!(
-    //    "The resulting averages are the following {:?}\nshould_be: {:?}\navg: {:?}\nmax: {:?}\nmin: {:?}",
-    //)
+        check(central.clone(), coefficients);
+    })
 }
 
 fn check(central: Mat<f64>, coefficients: Vec<Mat<f64>>) {
-    println!("{:?}", coefficients);
-    println!("central {:?}", central);
-    /*
-    res = all(
-        np.allclose(msg.coefficients, central.coefficients) for msg in coefficients_msgs
-    )
+    let res = coefficients
+        .iter()
+        .all(|coef| mat_allclose_default(coef, &central));
 
-    this_actor.info(f"Are the coefficients from every peer equal to central's? {res}")
-    */
+    println!("Are the coefficients from every peer equal to central's? {res}");
 }
