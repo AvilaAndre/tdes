fab229bad1b39c155706d440b8bf7d5273f47bd2
AvilaAndre <avila_andre14@hotmail.com>
2025-06-12 00:11:11 +0100

    refactor: move get_config to config.rs

--- src/internal/core/options/mod.rs
+++ src/internal/core/options/mod.rs
@@ -1,6 +1,8 @@
 use super::Context;
 use super::experiment::TopologyInfo;
 
+use serde_yaml::Value;
+
 pub mod arrival_time_registry;
 pub mod scenario_registry;
 pub mod topology_registry;
@@ -8,7 +10,6 @@ mod traits;
 
 pub use arrival_time_registry::ArrivalTimeRegistry;
 pub use scenario_registry::ScenarioRegistry;
-use serde_yaml::Value;
 pub use topology_registry::TopologyRegistry;
 pub use traits::{ArrivalTimeCallback, Scenario, Topology};
 
@@ -17,5 +18,5 @@ pub struct ExperimentOptions {
     pub topology: TopologyInfo,
     pub arrival_time: Option<String>,
     pub deadline: Option<f64>,
-    pub extra_args: Option<Value>
+    pub extra_args: Option<Value>,
 }
