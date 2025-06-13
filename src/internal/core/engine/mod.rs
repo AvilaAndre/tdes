e30ccd8b74298010d5b578553c1d468359213ec9
AvilaAndre <avila_andre14@hotmail.com>
2025-06-13 17:32:52 +0100

    refactor: make add_event not available outside of internal

--- src/internal/core/engine/mod.rs
+++ src/internal/core/engine/mod.rs
@@ -4,6 +4,7 @@ mod links;
 mod peers;
 
 pub use communication::send_message_to;
-pub use events::{add_event, add_timer, run};
+use events::add_event;
+pub use events::{add_timer, run};
 pub use links::{add_oneway_link, add_twoway_link};
 pub use peers::{add_peer, get_neighbors, get_neighbors_alive};
